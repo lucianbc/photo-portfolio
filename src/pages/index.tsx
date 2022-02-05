693f2f136504d35858e21065104a14dadf332712
lucianbc <lucianboaca97@gmail.com>
2022-02-05 17:57:21 +0000

    Removed who am i

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,12 @@ import { PhotoPreviewPortal } from "../components/PhotoPreviewPortal";
 export const query = graphql`
   query IndexPageQuery {
     allFile(
-      filter: { sourceInstanceName: { eq: "photos" } }
+      filter: {
+        sourceInstanceName: { eq: "photos" }
+        fields: { dimension: { aspectRatio: { eq: 1.5 } } }
+      }
       sort: { fields: fields___exif___exif___DateTimeOriginal, order: [DESC] }
+      limit: 6
     ) {
       nodes {
         childImageSharp {
@@ -174,26 +178,11 @@ const HeroLinks = styled.ul`
   }
 `;
 
-const WhoAmI = () => {
+const Highlights = ({ data }: { data: QueryResult }) => {
   return (
-    <section className="color-secondary">
-      <div
-        className="container-sm text-center content-font-size"
-        style={{
-          paddingTop: "90px",
-          paddingBottom: "90px",
-        }}
-      >
-        <h2>Who am I</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+    <section className="padded-vertical color-secondary">
+      <div className="container-md">
+        <PhotoGrid photos={data.allFile.nodes} />
       </div>
     </section>
   );
@@ -218,7 +207,8 @@ const IndexPage = ({ data }: { data: QueryResult }) => {
     <PhotoPreviewPortal>
       <ScrollHeader />
       <Hero data={data.heroPhoto} />
-      <WhoAmI />
+      <Highlights data={data} />
+      {/* <WhoAmI /> */}
       <RecentPosts data={data} />
       <Footer />
     </PhotoPreviewPortal>
