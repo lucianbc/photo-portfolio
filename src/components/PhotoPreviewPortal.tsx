1c99fc391d3c75b5b5aeb73d5f7f94514be96872
lucianbc <lucianboaca97@gmail.com>
2022-04-03 14:10:22 +0100

    Added click listeners

--- src/components/PhotoPreviewPortal.tsx
+++ src/components/PhotoPreviewPortal.tsx
@@ -1,6 +1,12 @@
 import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styled, { keyframes } from "styled-components";
 
 type Props = {
@@ -95,14 +101,56 @@ const PhotoPreview: React.FC<{ photo: ImageSharp }> = ({ photo }) => {
     };
   });
 
+  const photoNavRef = useRef<HTMLDivElement>(null);
+
   return (
-    <PhotoPreviewContainer onClick={closePortal}>
-      <GatsbyImage
-        image={getImage(photo as any)}
-        alt={photo.name}
-        style={{ maxWidth: "100%", maxHeight: "100%" }}
-        objectFit="contain"
-      />
+    <PhotoPreviewContainer>
+      <div className="close-row">
+        <span className="close-button" onClick={closePortal}>
+          <div>
+            <span className="bar one" />
+            <span className="bar two" />
+          </div>
+        </span>
+      </div>
+      <div
+        ref={photoNavRef}
+        className="photo-and-nav"
+        onClick={(e) => {
+          const rect = photoNavRef.current?.getBoundingClientRect();
+
+          if (rect) {
+            const position = { x: e.clientX - rect.x, y: e.clientY - rect.y };
+            const box = { width: rect.width, height: rect.height };
+            const isPrev = position.x < box.width / 2;
+            console.debug("event here", e.clientX, e.pageX, rect.x);
+          }
+        }}
+      >
+        <div className="nav-container">
+          <div className="nav-btn prev">
+            <span />
+            <span />
+          </div>
+        </div>
+        <div className="photo-box-wrapper">
+          <div className="photo-box">
+            <GatsbyImage
+              image={getImage(photo as any)}
+              alt={photo.name}
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+              objectFit="contain"
+            />
+          </div>
+        </div>
+
+        <div className="nav-container">
+          <div className="nav-btn next">
+            <span />
+            <span />
+          </div>
+        </div>
+      </div>
     </PhotoPreviewContainer>
   );
 };
@@ -145,10 +193,142 @@ const PhotoPreviewContainer = styled.div`
   right: 0;
   background-color: rgba(0, 0, 0, 1);
   z-index: 99999;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+
+  color: white;
+
+  & .close-row {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  & .close-button {
+    width: 25px;
+    height: 25px;
+
+    display: block;
+
+    position: relative;
+    z-index: 1;
+    padding: 30px;
+    margin: -30px;
+
+    & > div {
+      position: relative;
+      height: 100%;
+
+      cursor: pointer;
+
+      & > .bar {
+        width: 100%;
+        display: block;
+        height: 2px;
+        background-color: white;
+        position: absolute;
+
+        border-radius: 3px;
+
+        &.one {
+          top: 0;
+          transform: rotate(45deg);
+          transform-origin: left;
+        }
+
+        &.two {
+          bottom: 5.5px;
+          transform: rotate(-45deg);
+          transform-origin: left;
+        }
+      }
+    }
+  }
+
+  & .photo-and-nav {
+    display: flex;
+    width: 100%;
+    flex-grow: 1;
+  }
+
+  & .photo-box-wrapper {
+    position: relative;
+    flex-grow: 1;
+  }
+
+  & .photo-box {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & .nav-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & .nav-btn {
+      width: 30px;
+      height: 40px;
+
+      cursor: pointer;
+      position: relative;
+
+      & span {
+        position: absolute;
+        width: 100%;
+        background-color: white;
+        height: 2px;
+        border-radius: 2px;
+
+        & :first-child {
+          top: 0;
+        }
+
+        & :last-child {
+          bottom: 0;
+        }
+      }
+
+      &.next {
+        margin-left: 10px;
+
+        & span {
+          & :first-child {
+            transform: rotate(40.5deg);
+            transform-origin: left;
+          }
+
+          & :last-child {
+            transform: rotate(-40.5deg);
+            transform-origin: left;
+          }
+        }
+      }
+
+      &.prev {
+        margin-right: 10px;
+
+        & span {
+          & :first-child {
+            transform: rotate(-40.5deg);
+            transform-origin: right;
+          }
+
+          & :last-child {
+            transform: rotate(40.5deg);
+            transform-origin: right;
+          }
+        }
+      }
+    }
+  }
 
   opacity: 1;
   animation-name: ${fadeInAnim};
